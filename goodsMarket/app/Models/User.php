a4e816e12e89befa2d78218f086bb35aa41e3eb8
Dokal22 <stly3466@gmail.com>
2024-02-15 14:42:05 +0900

    회원가입, 로그인, 로그인시 출입처리

--- goodsMarket/app/Models/User.php
+++ goodsMarket/app/Models/User.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class user extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "u_name",
+        "u_nickname",
+        "u_email",
+        "u_pw",
+        "u_phone_num",
+        "u_agree_flg",
+    ];
 }
